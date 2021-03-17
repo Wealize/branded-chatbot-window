6a32c28c3757f9edeb8aa18d12933e29b9155310
Daniel Ranchal <daniranchal@wealize.digital>
2021-03-17 16:49:18 +0100

    more changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@
 // import Chat from './components/Chat'
 
 // ReactDOM.render(
-//   <Chat {...(window.chatbotSettings)}/>,
+//   <Chat 
+//     chatbotEndpoint='wss://cp.eu.ngrok.io/webchat/2bb31158-ff95-44c2-9f45-1d0f4b5ab02b/'
+//     welcomeMessage='¡Bienvenido al webchat de Tec!'
+//     startButton='¡Hola!'
+//     agentProfile={{
+//       teamName: 'TEC',
+//       imageUrl: 'https://tnpnlu-assets.s3.eu-central-1.amazonaws.com/Logo_TEC.png'
+//     }}
+//     theme={{
+//       brandColor: 'blue'
+//     }}
+//     userTimeout={3600}
+//     isWebView={true}
+//   />,
 //   document.getElementById('chatbotContainer')
 // )
 import Chat from './components/Chat';
