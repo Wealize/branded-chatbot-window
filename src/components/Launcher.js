16ecf648394a2bdf9cd9c2adaf0d2564cf189c60
Daniel Ranchal Parrado <daniranchal@wealize.digital>
2021-05-12 11:52:20 +0200

    Installing new version. Removing customization for now (#25)
    * Installing new version. Removing customization for now
    
    * Removing theme
    
    * Remove theme provider
    
    * Remove theme provider import
    
    * Updating changelog

--- src/components/Launcher.js
+++ src/components/Launcher.js
@@ -1,84 +1,24 @@
 import React from 'react'
-import Color from 'color'
-import styled, { css, ThemeProvider } from 'styled-components'
+
+import styled, { css } from 'styled-components'
 import { Launcher } from '@wealize/react-chat-window'
 
 const LauncherContainer = styled.div`
   ${(props) => {
-    const brandColor = Color(props.theme.brandColor);
-
     return css`
       & .sc-launcher {
-        background-color: ${brandColor.rgb().string()};
         z-index: 999999;
       }
 
       & .sc-chat-window {
         z-index: 999999;
       }
-
-      & .sc-header {
-        background-color: ${brandColor.rgb().string()};
-      }
-
-      & .sc-header--close-button:hover {
-        background-color: ${brandColor.darken(0.2).rgb().string()};
-      }
-
-      & .sc-message--content.sent .sc-message--text {
-        background-color: ${brandColor.rgb().string()};
-      }
-
-      & .sc-message--content.received .sc-message--text,
-      & .sc-user-input {
-        background-color: ${brandColor.lighten(0.8).alpha(0.3).rgb().string()};
-      }
-
-      & .sc-user-input input.sc-user-input--text {
-        background-color: initial;
-      }
-
-      & .sc-user-input.active {
-        background-color: white;
-      }
-
-      & .sc-message--img {
-        width: 90%;
-        height: auto;
-      }
-
-      & .sc-message--video {
-        width: 100%;
-        height: 100%;
-      }
-
-      & .sc-message--content.sent .sc-message--text {
-        background-color: ${brandColor.rgb().alpha(0.5).string()};
-      }
-
-      & .sc-quick-replies-block {
-        background-color: ${brandColor.lighten(0.8).alpha(0.3).rgb().string()};
-      }
-
-      & .sc-quick-reply-button {
-        border: 1px solid ${brandColor.rgb().string()};
-        color: ${brandColor.rgb().string()};
-        background-color: white;
-      }
-
-      & .sc-quick-reply-button:hover {
-        border: 1px solid white;
-        color: white;
-        background-color: ${brandColor.rgb().string()};
-      }
     `
   }}
 `
 
-export default ({ theme, ...props }) => (
-  <ThemeProvider theme={theme}>
-    <LauncherContainer>
-      <Launcher {...props} />
-    </LauncherContainer>
-  </ThemeProvider>
+export default ({ ...props }) => (
+  <LauncherContainer>
+    <Launcher {...props} />
+  </LauncherContainer>
 )
