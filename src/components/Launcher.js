0f3e56fd91fdb7119a3fcf3ac877933f6f705d8f
Daniel Ranchal Parrado <daniranchal@wealize.digital>
2021-05-26 13:15:54 +0200

    Adding theme again (#35)
    * Adding theme again
    
    * Launch symbol
    
    * Adding styles
    
    * Update changelog
    
    * Update changelog
    
    * Update lock

--- src/components/Launcher.js
+++ src/components/Launcher.js
@@ -1,24 +1,101 @@
 import React from 'react'
-
-import styled, { css } from 'styled-components'
+import Color from 'color'
+import styled, { css, ThemeProvider } from 'styled-components'
 import { Launcher } from '@wealize/react-chat-window'
 
 const LauncherContainer = styled.div`
   ${(props) => {
-    return css`
-      & .sc-launcher {
-        z-index: 999999;
-      }
+    if (props.theme.brandColor) {
+      const brandColor = Color(props.theme.brandColor)
 
-      & .sc-chat-window {
-        z-index: 999999;
-      }
-    `
+      return css`
+        & .sc-launcher {
+          z-index: 999999;
+          background-color: ${brandColor.rgb().string()};
+        }
+
+        & .sc-chat-window {
+          z-index: 999999;
+        }
+
+        & .sc-header {
+          background-color: ${brandColor.rgb().string()};
+        }
+
+        & .sc-header--close-button:hover {
+          background-color: ${brandColor.darken(0.2).rgb().string()};
+        }
+
+        & .sc-message--content.sent .sc-message--text {
+          background-color: ${brandColor.rgb().string()};
+        }
+
+        & .sc-message--content.received .sc-message--text {
+          background-color: ${brandColor.lighten(0.8).alpha(0.3).rgb().string()};
+        }
+
+        & .sc-message--content.sent .sc-message--text {
+          background-color: ${brandColor.rgb().alpha(0.5).string()};
+        }
+
+        & .sc-quick-reply-button {
+          border: 1px solid ${brandColor.rgb().string()};
+          color: ${brandColor.rgb().string()};
+          background-color: white;
+        }
+
+        & .sc-quick-reply-button:hover {
+          border: 1px solid white;
+          color: white;
+          background-color: ${brandColor.rgb().string()};
+        }
+
+        & .sc-user-input--send-button.active {
+          background-color: ${brandColor.lighten(0.8).alpha(0.3).rgb().string()};
+        }
+
+        & .sc-user-input--send-button.has-input:hover {
+          background-color: ${brandColor.rgb().string()};
+        }
+
+        & .background{
+            fill: ${brandColor.rgb().string()};
+        }
+
+        & .ear{
+            fill: ${brandColor.lighten(0.8).alpha(0.3).rgb().string()};
+        }
+
+        & .mouth{
+            fill: ${brandColor.lighten(0.8).alpha(0.3).rgb().string()};
+        }
+
+        & .hair {
+            fill: ${brandColor.lighten(0.8).alpha(0.3).rgb().string()};
+        }
+
+        & .shirt {
+            fill: ${brandColor.lighten(0.8).alpha(0.3).rgb().string()};
+        }
+      `
+    } else {
+      return css`
+        & .sc-launcher {
+          z-index: 999999;
+        }
+
+        & .sc-chat-window {
+          z-index: 999999;
+        }
+      `
+    }
   }}
 `
 
-export default ({ ...props }) => (
-  <LauncherContainer>
-    <Launcher {...props} />
-  </LauncherContainer>
+export default ({ theme, ...props }) => (
+  <ThemeProvider theme={theme}>
+    <LauncherContainer>
+      <Launcher {...props} />
+    </LauncherContainer>
+  </ThemeProvider>
 )
